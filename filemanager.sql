f8d57d59949edc84b3201cd33caebf33663c7e98
Andrew Guterman <andrew_guterman@yahoo.com>
2018-02-11 10:30:52 -0800

    Update filemanager.sql

--- filemanager.sql
+++ filemanager.sql
@@ -33,13 +33,6 @@ CREATE TABLE IF NOT EXISTS `accounts` (
   `email` varchar(256) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `accounts`
---
-
-INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES
-(1, 'Andrew', '47278066fd0526ed8ca9cb4da765fc0217ab2664bba446d20ecb1985d2ce04021abf2ea2963bbda1da2333b03f6bd8fbc100297c5cb0c1aa5ba9eb66cabd8785', 'andrew_guterman@yahoo.com');
-
 -- --------------------------------------------------------
 
 --
@@ -53,21 +46,6 @@ CREATE TABLE IF NOT EXISTS `files` (
   `name` varchar(128) NOT NULL,
   `fileid` varchar(16) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `files`
---
-
-INSERT INTO `files` (`id`, `user`, `path`, `name`, `fileid`) VALUES
-(1, 1, 'C:/xampp/uploads/c4ca4238a0b9', 'vacation.png', 'c4ca4238a0b9'),
-(2, 1, 'C:/xampp/uploads/c81e728d9d4c', 'buttsecks.png', 'c81e728d9d4c'),
-(3, 1, 'C:/xampp/uploads/eccbc87e4b5c', 'filemanager.jpg', 'eccbc87e4b5c'),
-(4, 1, 'C:/xampp/uploads/a87ff679a2f3', 'forums.png', 'a87ff679a2f3'),
-(5, 1, 'C:/xampp/uploads/e4da3b7fbbce', 'geolocator.png', 'e4da3b7fbbce'),
-(6, 1, 'C:/xampp/uploads/1679091c5a88', 'offtoyou.jpg', '1679091c5a88'),
-(7, 1, 'C:/xampp/uploads/8f14e45fceea', 'sflimo.jpg', '8f14e45fceea'),
-(8, 1, 'C:/xampp/uploads/c9f0f895fb98', 'ucp.jpg', 'c9f0f895fb98'),
-(9, 1, 'C:/xampp/uploads/45c48cce2e2d', 'lawless.txt', '45c48cce2e2d');
 
 --
 -- Indexes for dumped tables
